e35bf8e4a5306b9fa6c68df3139bc3eee77a3147
FilipaRamos <fimamora@gmail.com>
2015-05-14 16:43:29 +0100

    Files

---
+++ src/Files.cpp
@@ -0,0 +1,10 @@
+/*
+ * Files.cpp
+ *
+ *  Created on: 14/05/2015
+ *      Author: Filipa
+ */
+
+#include "Files.h"
+
+
---
+++ src/Files.h
@@ -0,0 +1,18 @@
+/*
+ * Files.h
+ *
+ *  Created on: 14/05/2015
+ *      Author: Filipa
+ */
+
+#ifndef FILES_H_
+#define FILES_H_
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+
+
+
+#endif /* FILES_H_ */
