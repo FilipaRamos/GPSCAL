9fadef6f4fe0f428e8e3347e8edb6b047380b9c4
marianagoliveira <mariana.oliveira@sapo.pt>
2015-06-01 00:24:46 +0100

    PrefixTree usável

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,15 @@
 #include "StringMatching.h"
 #include "Files.h"
 #include "Morada.h"
+#include "PrefixTree.h"
 
 using namespace std;
 
 int main(){
 
+	PrefixTree pf;
+	vector<string> info;
+	vector<string> result;
 	string pesquisa;
 	int opcao;
 
@@ -41,14 +45,24 @@ int main(){
 
 	cin >> opcao;
 
+	File f = File("teste.txt");
+	f.readFile();
+
 	switch(opcao){
 
 	case 1:
-		File f = File("teste.txt");
-		f.readFile();
 		KnuthMorrisPratt(f.getMoradas(), pesquisa);
 		break;
 	case 2:
+		info = f.getMoradas();
+		for (int i = 0; i < info.size(); i++){
+			pf.insert(info[i]);
+		}
+		result = pf.complete(pesquisa);
+		for (int j = 0; j < result.size(); j++){
+			cout << result[j] << endl;
+		}
+
 		break;
 	default:
 		break;
