9bcfa2f78c52c37172684d941bec954ebd7b6c8c
FilipaRamos <fimamora@gmail.com>
2015-05-31 21:15:58 +0100

    text file in wrong location... afinal nao era erro

--- src/Files.cpp
+++ src/Files.cpp
@@ -16,16 +16,17 @@ void File::readFile(Moradas m){
 	string linha;
 	ifstream file;
 	file.open(file_name.c_str());
-
+	cout << "before!" << endl;
 	if(file.is_open()){
+		cout << "open" << endl;
 		while(!file.eof()){
 			getline(file, linha);
+			cout << linha << endl;
 			m.addMoradas(linha);
 		}
 	}
 	/*else
 		cout << "ERROR READING FILE!" << endl;
 */
-	file.close();
 }
 
